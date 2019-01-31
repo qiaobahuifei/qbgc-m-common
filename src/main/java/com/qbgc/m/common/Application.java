b944df0a68dd37095185c99cb5d067e87714db36
gaochao <gaochao5kuba@163,com>
2019-01-31 18:11:51 +0800

    修改为yml和删除不必要的文件

--- src/main/java/com/qbgc/m/common/Application.java
+++ src/main/java/com/qbgc/m/common/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class QbgcMCommonApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(QbgcMCommonApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
