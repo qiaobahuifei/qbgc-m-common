489a37c5fdf1818e0c10ac179a2986a03cec5a25
gaochao <gaochao5kuba@163,com>
2019-01-31 17:35:59 +0800

    first

---
+++ src/main/java/com/qbgc/m/common/QbgcMCommonApplication.java
@@ -0,0 +1,14 @@
+package com.qbgc.m.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QbgcMCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QbgcMCommonApplication.class, args);
+	}
+
+}
+
